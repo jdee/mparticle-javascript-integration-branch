32c10a946f2d0079c4dfc074f5bb312b45518322
Jimmy Dee <jimmy.dee@branch.io>
2021-02-18 15:29:33 -0800

    For evaluation purposes: Allow loading Branch SDK from node_modules or synchronously.

--- dist/BranchMetrics-Kit.common.js
+++ dist/BranchMetrics-Kit.common.js
@@ -2,6 +2,10 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var branchSdk = _interopDefault(require('branch-sdk'));
+
 function Common() {}
 
 Common.prototype.exampleMethod = function () {
@@ -206,13 +210,42 @@ var identityHandler = IdentityHandler;
 var initialization = {
     name: 'BranchMetrics',
     initForwarder: function(settings, testMode, userAttributes, userIdentities, processEvent, eventQueue, isInitialized, common) {
-        common.settings = settings;
+        var requireBranch = true;
+
         if (!testMode) {
-            if (!!window.branch) {
-                return;
+            var branch;
+            console.log('Determining Branch SDK source');
+            if (!!window && !requireBranch) {
+                if (!!window.branch) {
+                    console.log('Branch SDK already initialized');
+                    return;
+                }
+
+                var scriptUrl = 'https://cdn.branch.io/branch-latest.min.js';
+                /*
+                 * Pass { synchronousLoad: true } in settings to add a script
+                 * tag directly to synchronously load the Branch Web SDK from
+                 * CDN.
+                 */
+                {
+                    /*
+                     * Load asynchronously.
+                     */
+                    console.log('Loading Branch SDK asynchronously from ' + scriptUrl);
+                    (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src=scriptUrl;k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h;}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments]);};},{_q:[],_v:1},"addListener applyCode autoAppIndex banner closeBanner closeJourney creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode trackCommerceEvent logEvent disableTracking getBrowserFingerprintId crossPlatformIds lastAttributedTouchData".split(" "), 0);
+                }
+
+                branch = window.branch;
+            }
+            else {
+                /*
+                 * Load NPM dependency.
+                 */
+                console.log('Loading Branch SDK from node_modules');
+                branch = branchSdk;
             }
 
-            (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h;}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments]);};},{_q:[],_v:1},"addListener applyCode autoAppIndex banner closeBanner closeJourney creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode trackCommerceEvent logEvent disableTracking getBrowserFingerprintId crossPlatformIds lastAttributedTouchData".split(" "), 0);
+            console.log('Initializing Branch SDK');
             branch.init(settings.branchKey, function(err, data) {
             });
         }
--- src/initialization.js
+++ src/initialization.js
@@ -1,13 +1,54 @@
 var initialization = {
     name: 'BranchMetrics',
     initForwarder: function(settings, testMode, userAttributes, userIdentities, processEvent, eventQueue, isInitialized, common) {
-        common.settings = settings
+        var requireBranch = true;
+        var synchronousLoad = false;
+
         if (!testMode) {
-            if (!!window.branch) {
-                return;
+            var branch;
+            console.log('Determining Branch SDK source');
+            if (!!window && !requireBranch) {
+                if (!!window.branch) {
+                    console.log('Branch SDK already initialized');
+                    return;
+                }
+
+                var scriptUrl = 'https://cdn.branch.io/branch-latest.min.js';
+                /*
+                 * Pass { synchronousLoad: true } in settings to add a script
+                 * tag directly to synchronously load the Branch Web SDK from
+                 * CDN.
+                 */
+                if (!!synchronousLoad) {
+                    /*
+                     * Load synchronously
+                     */
+                    console.log('Loading Branch SDK synchronously from ' + scriptUrl);
+                    var scriptTag = document.createElement('script');
+                    scriptTag.async = 0;
+                    scriptTag.src = scriptUrl;
+                    var firstScript = document.getElementByTagName('script')[0];
+                    firstScript.parentNode.insertBefore(scriptTag, firstScript);
+                }
+                else {
+                    /*
+                     * Load asynchronously.
+                     */
+                    console.log('Loading Branch SDK asynchronously from ' + scriptUrl);
+                    (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src=scriptUrl;k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode autoAppIndex banner closeBanner closeJourney creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode trackCommerceEvent logEvent disableTracking getBrowserFingerprintId crossPlatformIds lastAttributedTouchData".split(" "), 0);
+                }
+
+                branch = window.branch;
+            }
+            else {
+                /*
+                 * Load NPM dependency.
+                 */
+                console.log('Loading Branch SDK from node_modules');
+                branch = require('branch-sdk');
             }
 
-            (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode autoAppIndex banner closeBanner closeJourney creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode trackCommerceEvent logEvent disableTracking getBrowserFingerprintId crossPlatformIds lastAttributedTouchData".split(" "), 0);
+            console.log('Initializing Branch SDK');
             branch.init(settings.branchKey, function(err, data) {
                 isInitialized = true;
             });
